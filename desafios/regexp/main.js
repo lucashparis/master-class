f9bad2e93ec1e5c5919510067bc4679b95160c4f
Lucas Paris <lucashenriqueparis@gmail.com>
2022-01-19 00:27:36 -0300

    JSON challenge

--- desafios/regexp/main.js
+++ desafios/regexp/main.js
@@ -3,7 +3,7 @@ const regexp = /create table ([a-z]+) \((.+)\)/;
 const parsedStatement = statement.match(regexp);
 const tableName = parsedStatement[1];
 let columns = parsedStatement[2];
-columns = columns.split("; ")
+columns = columns.split("; ");
 
 console.log("tableName: ", tableName);
 console.log("columns: ", columns);
